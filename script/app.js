0b2dc2d9b695cc91aa8b81734e9dc538aad4b14a
Katrien S <66075391+graficdoctor@users.noreply.github.com>
2021-02-07 17:50:58 +0100

    Fixed mistake in the script

--- script/app.js
+++ script/app.js
@@ -1,28 +1,33 @@
 const form = document.getElementById('form')
-const email = document.getElementById('email').value.trim()
+const email = document.getElementById('email')
 const message = document.querySelector('.message')
 const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-  checkInputs()
+  const inputtedValue = email.value.trim()
+  checkInputs(inputtedValue)
 })
 
-function checkInputs() {
-  if (!email) {
+function checkInputs(inputtedValue) {
+  if (inputtedValue === '') {
     message.classList.remove('valid')
     message.classList.add('invalid')
     message.innerText = 'You forgot to enter your address'
     message.style.display = 'inline-block'
-  } else if (email.match(emailPattern)) {
+    form.reset()
+    message.reset()
+  } else if (inputtedValue.match(emailPattern)) {
     message.classList.add('valid')
     message.classList.remove('invalid')
     message.innerText = 'Your email address is valid'
     message.style.display = 'inline-block'
+    form.reset()
   } else {
     message.classList.remove('valid')
     message.classList.add('invalid')
     message.innerText = 'Oops, something went wrong'
     message.style.display = 'inline-block'
+    form.reset()
   }
 }
